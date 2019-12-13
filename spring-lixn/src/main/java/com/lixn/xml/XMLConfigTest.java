dbcfe80a910b36d2f8ff2adf28ac6c00c151f9d6
loveYu1314 <736881321@qq.com>
2019-12-13 09:26:57 +0800

    Spring自动装配源码分析

--- spring-lixn/src/main/java/com/lixn/xml/XMLConfigTest.java
+++ spring-lixn/src/main/java/com/lixn/xml/XMLConfigTest.java
@@ -23,18 +23,18 @@ public class XMLConfigTest {
 //		System.out.println(bean.getXmlStr());
 
 		// 根据Xml配置文件创建Resource资源对象，该对象包含了BeanDefinition的信息
-		ClassPathResource resource = new ClassPathResource("spring-config.xml");
+//		ClassPathResource resource = new ClassPathResource("spring-config.xml");
 		// 创建DefaultListableBeanFactory
-		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+//		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 		// 创建 XmlBeanDefinitionReader 读取器，用于载入BeanDefinition
 		// BeanFactory作为参数，是为了将读取的信息回调配置给factory
-		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+//		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
 		// XmlBeanDefinitionReader执行载入BeanDefinition的方法，最后会完成Bean的载入和注册
 		// 完成后Bean就成功的放置到IOC容器中了，
-		reader.loadBeanDefinitions(resource);
+//		reader.loadBeanDefinitions(resource);
 
 
-		ApplicationContext context = new FileSystemXmlApplicationContext("spring-config.xml");
+//		ApplicationContext context = new FileSystemXmlApplicationContext("spring-config.xml");
 
 		ClassPathXmlApplicationContext xmlApplicationContext =
 				new ClassPathXmlApplicationContext("spring-config.xml");
